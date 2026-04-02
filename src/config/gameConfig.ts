dbb3e854c912b872206389322b2ededab6c70e83
LuckyDev2121 <bincheri826@gmail.com>
2026-04-02 20:26:19 +0800

    update connect error3

--- src/config/gameConfig.ts
+++ src/config/gameConfig.ts
@@ -7,9 +7,10 @@ export const GAME_DETAILS_CHANNEL =
   import.meta.env.VITE_GAME_DETAILS_CHANNEL ?? "game-channel";
 export const GAME_DETAILS_EVENT =
   import.meta.env.VITE_GAME_DETAILS_EVENT ?? "game.updated";
-export const PUSHER_APP_KEY = import.meta.env.VITE_PUSHER_APP_KEY ?? "";
+export const PUSHER_APP_KEY =
+  import.meta.env.VITE_PUSHER_APP_KEY ?? "k6dbocgucm0at6gwak3y";
 export const PUSHER_HOST = import.meta.env.VITE_PUSHER_HOST ?? "funint.site";
-export const PUSHER_WS_PORT = Number(import.meta.env.VITE_PUSHER_WS_PORT ?? 6001);
+export const PUSHER_WS_PORT = Number(import.meta.env.VITE_PUSHER_WS_PORT ?? 8080);
 export const PUSHER_WSS_PORT = Number(import.meta.env.VITE_PUSHER_WSS_PORT ?? 443);
 export const PUSHER_FORCE_TLS =
   import.meta.env.VITE_PUSHER_FORCE_TLS !== "false";
