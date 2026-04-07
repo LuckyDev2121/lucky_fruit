7e99429e7b911c56aac7adcacc86d6511f6d275d
LuckyDev2121 <bincheri826@gmail.com>
2026-04-07 22:13:38 +0800

    update 141

--- src/components/ChooseRectangle.tsx
+++ src/components/ChooseRectangle.tsx
@@ -50,7 +50,7 @@ export default function ChooseRectangle({ onChooseTimeUp }: { onChooseTimeUp?: (
                 setTimestep(100);
             }
             if (second > 2000 && second < 3820) {
-                if (second === 2100) { console.log("steps", makeResult?.winning_option_id, "====", steps, "====", currentFruit.id); }
+                if (second === 2100) { console.log(makeResult, makeResult?.winning_option_id, "====", steps, "====", currentFruit.id); }
                 if (steps === 0) {
                     if (second > 2200) {
                         setSecond((s) => s + 100 + 100 * add);
