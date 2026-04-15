470d07492560a190b15d35638d80379ea3014271
LuckyDev2121 <bincheri826@gmail.com>
2026-04-16 02:12:58 +0800

    update 195

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/lucky-fruits/",   // 👈 THIS IS REQUIRED
+  // base: "/lucky-fruits/",   // 👈 THIS IS REQUIRED
   plugins: [react()],
   server: {
     proxy: {
